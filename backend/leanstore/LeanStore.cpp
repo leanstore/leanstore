e4af259e0599b7c011075c4a6d83b02a947f8b61
Adnan Alhomssi <a.homssi@gmail.com>
2019-12-17 10:54:14 +0100

    * remove unnecessary dependency

--- backend/leanstore/LeanStore.cpp
+++ backend/leanstore/LeanStore.cpp
@@ -2,8 +2,6 @@
 #include "leanstore/utils/FVector.hpp"
 // -------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------
-#include "spdlog/sinks/rotating_file_sink.h"
-#include "spdlog/spdlog.h"
 #include "gflags/gflags.h"
 // -------------------------------------------------------------------------------------
 DEFINE_bool(log_stdout, false, "");
@@ -13,11 +11,6 @@ namespace leanstore {
 LeanStore::LeanStore()
 {
    // Set the default logger to file logger
-   if ( !FLAGS_log_stdout ) {
-      auto file_logger = spdlog::rotating_logger_mt("main_logger", "log.txt",
-                                                    1024 * 1024 * 10, 3);
-      spdlog::set_default_logger(file_logger);
-   }
    BMC::global_bf = &buffer_manager;
    buffer_manager.registerDatastructureType(99, btree::vs::BTree::getMeta());
 }
