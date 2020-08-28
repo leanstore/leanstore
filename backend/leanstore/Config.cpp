6160229750efadae052fdfb8ef5c5e6381c7cf6e
Adnan Alhomssi <a.homssi@gmail.com>
2020-08-28 16:40:49 +0200

    fix contention split ratio

--- backend/leanstore/Config.cpp
+++ backend/leanstore/Config.cpp
@@ -39,7 +39,7 @@ DEFINE_uint64(warmup_for_seconds, 10, "Warmup for x seconds");
 DEFINE_bool(cm_split, false, "");
 DEFINE_uint64(cm_update_on, 7, "as exponent of 2");
 DEFINE_uint64(cm_period, 14, "as exponent of 2");
-DEFINE_uint64(cm_slowpath_threshold, 1, "");
+DEFINE_uint64(cm_slowpath_threshold, 100, "");
 // -------------------------------------------------------------------------------------
 DEFINE_bool(su_merge, false, "");
 DEFINE_uint64(su_kwaymerge, 5, "");
