d98e7929cc57ff182fcb440f4db40cdb3df0fd99
Adnan Alhomssi <a.homssi@gmail.com>
2021-01-11 13:22:04 +0100

    clean up

--- docs/experiments/tpcc/tpcc_overnight.r
+++
@@ -1,5 +0,0 @@
-library(ggplot2)
-library(sqldf)
-
-pcsv=read.csv('/home/adnan/rome/dev/leanstore/docs/experiments/tpcc/tpcc_overnight.csv')
-ggplot(pcsv,aes(x=t, y=tx, color = c_cm_split, group=c_cm_split)) + geom_line() + facet_grid(row=vars(c_zipf_factor,tpcc_warehouse_count), cols=vars(c_worker_threads))
--- docs/experiments/ycsb/cm/plot.r
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-library(sqldf)
-
-
-# Rome
-df=read.csv('/home/adnan/rome/dev/leanstore/docs/experiments/ycsb/cm/results.csv')
-d=sqldf("select * from df")
-tx <- ggplot(d, aes(t, tx, color=c_cm_split, group=c_cm_split)) + geom_line()
-tx + facet_grid (row=vars(c_zipf_factor), cols=vars(ycsb_read_ratio))
