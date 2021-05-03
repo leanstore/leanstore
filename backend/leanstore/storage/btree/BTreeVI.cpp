203083982836e4eb4fd04d22b2152ff80916f2fb
Adnan Alhomssi <a.homssi@gmail.com>
2021-05-03 15:20:08 +0000

    vi: l1-miss free pgc

--- backend/leanstore/storage/btree/BTreeVI.cpp
+++ backend/leanstore/storage/btree/BTreeVI.cpp
@@ -171,12 +171,11 @@ OP_RESULT BTreeVI::updateSameSizeInPlace(u8* o_key,
          cr::Worker::my().sortWorkers();  // TODO: 200 L1 miss!
          SN prev_sn = 0, cur_sn = gc_next_sn;
          u64 w = 0;
-         const u64 primary_version_commited_before_so = cr::Worker::my().getCB(primary_version_worker_id, primary_version_commited_after_so);
-         auto is_current_worker_so_larger = [&](const u64 so) {
+         auto should_we_keep_this_version = [&](const u64 commited_after_so) {
             const u64 cur_w = cr::Worker::my().all_sorted_so_starts[w] & cr::Worker::WORKERS_MASK;
-            return (cr::Worker::my().all_so_starts[cur_w]) > so;
+            return (cr::Worker::my().all_so_starts[cur_w]) > commited_after_so;
          };
-         while (w < cr::Worker::my().workers_count && is_current_worker_so_larger(primary_version_commited_before_so)) {
+         while (w < cr::Worker::my().workers_count && should_we_keep_this_version(primary_version_commited_after_so)) {
             w++;
          }
          WorkerCounters::myCounters().cc_update_chains_pgc_skipped[dt_id] += w;
@@ -194,12 +193,11 @@ OP_RESULT BTreeVI::updateSameSizeInPlace(u8* o_key,
             ensure(!gc_version.is_removed);
             gc_next_sn = gc_version.next_sn;
             COUNTERS_BLOCK() { WorkerCounters::myCounters().cc_update_chains_pgc_workers_visited[dt_id]++; }
-            if (cr::Worker::my().isVisibleForIt(cr::Worker::my().all_sorted_so_starts[w] & cr::Worker::WORKERS_MASK, gc_version.worker_id,
-                                                gc_version.tts)) {
+            if (should_we_keep_this_version(gc_version.commited_after_so)) {
                COUNTERS_BLOCK() { WorkerCounters::myCounters().cc_update_versions_kept[dt_id]++; }
                w++;
                while (gc_next_sn != 0 && w < cr::Worker::my().workers_count) {
-                  if (is_current_worker_so_larger(gc_version.commited_before_so)) {
+                  if (should_we_keep_this_version(gc_version.commited_after_so)) {
                      w++;
                      WorkerCounters::myCounters().cc_update_chains_pgc_skipped[dt_id]++;
                   } else {
