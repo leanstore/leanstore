5c69dfaacad104ef1b761b9a4f8320bcc15c4288
Adnan Alhomssi <a.homssi@gmail.com>
2022-02-12 00:19:06 +0100

    enable contention split by default

--- backend/leanstore/Config.cpp
+++ backend/leanstore/Config.cpp
@@ -34,7 +34,7 @@ DEFINE_double(target_gib, 0.0, "size of dataset in gib (exact interpretation dep
 DEFINE_uint64(run_for_seconds, 10, "Keep the experiment running for x seconds");
 DEFINE_uint64(warmup_for_seconds, 10, "Warmup for x seconds");
 // -------------------------------------------------------------------------------------
-DEFINE_bool(contention_split, false, "");
+DEFINE_bool(contention_split, true, "");
 DEFINE_uint64(cm_update_on, 7, "as exponent of 2");
 DEFINE_uint64(cm_period, 14, "as exponent of 2");
 DEFINE_uint64(cm_slowpath_threshold, 1, "");
