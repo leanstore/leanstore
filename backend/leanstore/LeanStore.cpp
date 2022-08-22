bf8f0d9a31bd48cd26ed7b9f291e49ed10a9dfc3
Adnan Alhomssi <a.homssi@gmail.com>
2022-08-22 18:03:40 +0200

    add LLC/TX counter to console

--- backend/leanstore/LeanStore.cpp
+++ backend/leanstore/LeanStore.cpp
@@ -190,15 +190,16 @@ void LeanStore::startProfilingThread()
          const double instr_per_tx = cpu_table.workers_agg_events["instr"] / tx;
          const double cycles_per_tx = cpu_table.workers_agg_events["cycle"] / tx;
          const double l1_per_tx = cpu_table.workers_agg_events["L1-miss"] / tx;
+         const double llc_per_tx = cpu_table.workers_agg_events["LLC-miss"] / tx;
          // using RowType = std::vector<variant<std::string, const char*, Table>>;
          if (FLAGS_print_tx_console) {
             tabulate::Table table;
-            table.add_row({"t", "OLTP TX", "RF %", "Abort%", "OLAP TX", "W MiB", "R MiB", "Instrs/TX", "Cycles/TX", "CPUs", "L1/TX", "GHz",
+            table.add_row({"t", "OLTP TX", "RF %", "Abort%", "OLAP TX", "W MiB", "R MiB", "Instrs/TX", "Cycles/TX", "CPUs", "L1/TX", "LLC/TX", "GHz",
                            "WAL GiB/s", "GCT GiB/s", "Space G", "GCT Rounds"});
             table.add_row({std::to_string(seconds), std::to_string(tx), std::to_string(remote_flushes_pct), std::to_string(tx_abort_pct),
                            std::to_string(olap_tx), bm_table.get("0", "w_mib"), bm_table.get("0", "r_mib"), std::to_string(instr_per_tx),
                            std::to_string(cycles_per_tx), std::to_string(cpu_table.workers_agg_events["CPU"]), std::to_string(l1_per_tx),
-                           std::to_string(cpu_table.workers_agg_events["GHz"]), cr_table.get("0", "wal_write_gib"),
+                           std::to_string(llc_per_tx), std::to_string(cpu_table.workers_agg_events["GHz"]), cr_table.get("0", "wal_write_gib"),
                            cr_table.get("0", "gct_write_gib"), bm_table.get("0", "space_usage_gib"), cr_table.get("0", "gct_rounds")});
             // -------------------------------------------------------------------------------------
             table.format().width(10);
