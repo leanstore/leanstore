302e525e46dc55d98d54fb7aa5f491af1c113df7
Duy Nguyen <lamduy.nguyen@tum.de>
2023-11-27 09:23:29 +0100

    fresh start

--- backend/leanstore/profiling/counters/WorkerCounters.cpp
+++
@@ -1,9 +0,0 @@
-#include "WorkerCounters.hpp"
-// -------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------
-namespace leanstore
-{
-atomic<u64> WorkerCounters::workers_counter = 0;
-tbb::enumerable_thread_specific<WorkerCounters> WorkerCounters::worker_counters;
-}  // namespace leanstore
